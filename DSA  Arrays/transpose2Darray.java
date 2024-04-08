a18f70418fa485aeaadbd560813de30d6149a52b
Tarushi Arora <91682167+tarushiarora@users.noreply.github.com>
2024-04-08 11:34:20 +0530

    Update transpose2Darray.java

--- DSA  Arrays/transpose2Darray.java
+++ DSA  Arrays/transpose2Darray.java
@@ -4,25 +4,32 @@ import java.util.*;
 public class transpose2Darray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // n rows
-        int m = sc.nextInt(); // m columns
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
 
-        int [][] arr = new int[n][m];
-        //input of 2D array
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int [][] arr = new int[rows][cols];
+        //input
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
                 arr[i][j] = sc.nextInt();
             }
         }
-        System.out.println("The transpose is : ");
 
-        // to print transpose
-        for(int j=0;j<m;j++){
-            for (int i=0;i<n;i++){
+        //output
+        System.out.println("original 2d array is:");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
-
+        // 
+        System.out.println("Transpose is");
+        for(int j=0;j<cols;j++){
+            for(int i=0;i<rows;i++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
